2fab5ef5aaf87637b4a89b302eb0cc646ed3acae
Hanson Pereira <hanson@microcentergulf.com>
2025-06-07 08:14:09 -0400

    After Task 4

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,21 +4,21 @@ import requests
 def emotion_detector(text_to_analyze):
     # Define the URL for the sentiment analysis API
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-
     # Set the headers with the required model ID for the API
     header = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-
     # Create the payload with the text to be analyzed
     myobj = {"raw_document": { "text": text_to_analyze} }
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
     # Parse the response from the API
     formatted_response = json.loads(response.text)
-
     output = formatted_response["emotionPredictions"][0]["emotion"]
-    name = formatted_response["producerId"]["name"]
-    output.update({"dominant_emotion": name})
-    pretty_output = json.dumps(output, indent=4, sort_keys=False)
 
-    return output
+    #sort to find dominent name
+    numeric_items = {k: v for k, v in output.items() if isinstance(v, (int, float))}
+    sorted_data = dict(sorted(numeric_items.items(), key=lambda item: item[1], reverse=True))
+    dominent_name = list(sorted_data.keys())[0]
+    output.update({"dominant_emotion": dominent_name})
+    pretty_output = json.dumps(output, indent=4, sort_keys=False)
+    return pretty_output
